cd025b6f88593887efc9653e0c718fda1c6e36db
Obada Kadri <abu3antar@gmail.com>
2015-06-07 00:13:24 -0700

    Enhancements to login screen

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -12,6 +12,7 @@ angular.module('mealPlannerApp')
   ['$scope', '$location', 'currentAuth',
   function ($scope, $location, currentAuth) {
     var ref = new Firebase(FIREBASE_URL);
+    $scope.signInBtn = "Sign in";
 
     if (currentAuth) {
       console.log("already logged in, redirecting to main");
@@ -19,15 +20,20 @@ angular.module('mealPlannerApp')
     }
 
     $scope.login = function() {
+      $scope.signInBtn = "Signing in...";
       ref.authWithPassword({
         email    : $scope.email,
         password : $scope.password
       }, function(error, authData) {
         if (error) {
+          $scope.signInBtn = "Sign in";
+          $scope.message = "Login failed, please try again.";
           console.log("Login Failed!", error);
         } else {
-          $location.path("/");
+          $location.path("/").replace();
+          console.log("Login successful");
         }
+        $scope.$apply();
       });
     };
   }]);
