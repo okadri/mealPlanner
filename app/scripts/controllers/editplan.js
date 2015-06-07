1d6cade0c987409d9d37794ceb73b0c9429d7bf5
Obada Kadri <abu3antar@gmail.com>
2015-06-06 23:15:19 -0700

    Meal dropdown filter

--- app/scripts/controllers/editplan.js
+++ app/scripts/controllers/editplan.js
@@ -10,11 +10,21 @@
 angular.module('mealPlannerApp')
   .controller('EditplanCtrl',
   ['$scope', '$modalInstance', 'mealService', 'planService', 'mealId', 'plan', 'allMeals',
-  this.EditplanCtrl = function ($scope, $modalInstance, mealService, planService, mealId, plan, allMeals) {
+  this.EditplanCtrl = function ($scope, $modalInstance, mealService, planService, mealId, plan, allMeals, meatTypeService) {
     $scope.allMeals = allMeals;
     $scope.mealId = mealId;
+    $scope.meatTypes = meatTypeService.getAll();
 
     $scope.meal = mealService.getOneById(mealId)
+
+    $scope.filterMeatType = function(meatId) {
+      meatId = meatId.toString();
+      if ($scope.selectedMeatType.meatId === meatId) {
+        $scope.selectedMeatType = {};
+      } else {
+        $scope.selectedMeatType.meatId = meatId;
+      }
+    };
 
     $scope.setMeal = function() {
       $modalInstance.close($scope.meal);
