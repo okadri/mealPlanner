b9b6327d7a48d8fe7668aabb782fb70989731fdc
Obada Kadri <abu3antar@gmail.com>
2015-06-07 19:51:46 -0700

    Update 2015-06-08T02:51:45.276Z

--- scripts/scripts.3d62a3f4.js
+++ scripts/scripts.3d62a3f4.js
@@ -864,6 +864,7 @@ angular.module('mealPlannerApp')
     };
 
     $scope.addItem = function(item) {
+      item = item.charAt(0).toUpperCase() + item.slice(1);
       shoppingListService.addItem(item);
       $scope.newItem = '';
     };
@@ -988,3 +989,28 @@ angular.module('mealPlannerApp')
       });
     };
   }]);
+
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name mealPlannerApp.directive:onEnter
+ * @description
+ * # onEnter
+ */
+angular.module('mealPlannerApp')
+  .directive('onEnter', function () {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+          if (event.which === 13) { // Enter
+            scope.$apply(function() {
+              scope.$eval(attrs.onEnter);
+              event.preventDefault();
+            });
+          }
+        });
+      }
+    };
+  });
